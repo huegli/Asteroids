e019a495de8ba78ea6e0029afa5bfb2be0a299ed
Nikolai Schlegel <nikolai.schlegel@gmail.com>
2022-10-09 09:13:52 -0700

    Completed Asteroids & Spaceship

--- space_rocks/models.py
+++ space_rocks/models.py
@@ -1,7 +1,7 @@
 from pygame.math import Vector2
 from pygame.transform import rotozoom
 
-from utils import load_sprite
+from utils import get_random_velocity, load_sprite, wrap_position
 
 UP = Vector2(0, -1)
 
@@ -16,8 +16,8 @@ class GameObject:
         blit_position = self.position - Vector2(self.radius)
         surface.blit(self.sprite, blit_position)
 
-    def move(self):
-        self.position = self.position + self.velocity
+    def move(self, surface):
+        self.position = wrap_position(self.position + self.velocity, surface)
 
     def collides_with(self, other_obj):
         distance = self.position.distance_to(other_obj.position)
@@ -26,6 +26,7 @@ class GameObject:
 
 class SpaceShip(GameObject):
     MANEUVERABILITY = 3
+    ACCELERATION = 0.25
 
     def __init__(self, position):
         # Make a copy of the original UP vector
@@ -43,3 +44,13 @@ class SpaceShip(GameObject):
         rotated_surface_size = Vector2(rotated_surface.get_size())
         blit_position = self.position - rotated_surface_size * 0.5
         surface.blit(rotated_surface, blit_position)
+
+    def accelerate(self):
+        self.velocity += self.direction * self.ACCELERATION
+
+
+class Asteroid(GameObject):
+    def __init__(self, position):
+        super().__init__(
+            position, load_sprite("asteroid"), get_random_velocity(1, 3)
+        )
